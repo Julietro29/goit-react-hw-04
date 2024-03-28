17292cb82a83a95fe3774245fccc0e10872c1fac
Julietro29 <mousemiu1@gmail.com>
2024-03-29 00:32:53 +0200

    Add all tasks HW4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,88 @@
-import userData from '../userData.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import { useEffect, useState } from "react";
+import clsx from 'clsx';
+import Modal from 'react-modal';
 
-import { Profile } from './Profile/Profile';
-import { FriendList } from './Friends/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { SearchBar } from './SearchBar/SearchBar';
+import { ErrorMessage } from './ErrorMessage/ErrorMessage';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Loader } from './Loader/Loader';
+import { LoadMoreBtn } from './LoadMoreBtn/LoadMoreBtn';
+import { ImageModal } from './ImageModal/ImageModal';
 
-export default function App() {
-  const { username, tag, location, avatar, stats } = userData;
+import { fetchData } from '../api/fetch-data';
+import styles from './App.module.css';
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [currPage, setCurrPage] = useState(0);
+  const [hasMorePages, setHasMorePages] = useState(false);
+  const [filter, setFilter] = useState('');
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    Modal.setAppElement('#root');
+  }, []);
+
+  const updateImages = async (strFilter, page) => {
+    try {
+      setError(false);
+      setLoading(true);
+      const data = await fetchData(strFilter, page);
+      if (data.results.length > 0) {
+        setItems(prevItems => [...prevItems, ...data.results]);
+        setCurrPage(page);
+        setHasMorePages(page >= data.total_pages ? false : true);
+      } else {
+        setError(true);
+        setHasMorePages(false);
+      }
+    } catch (error) {
+      setError(true);
+      setHasMorePages(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = strFilter => {
+    setFilter(strFilter);
+    setItems([]);
+    setCurrPage(0);
+    setHasMorePages(false);
+    updateImages(strFilter, 1);
+  };
+
+  const handleMore = () => updateImages(filter, currPage + 1);
+  const openModal = image => {
+    setSelectedImage(image);
+    setModalIsOpen(true);
+  };
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
   return (
-    <div className="container">
-      <Profile
-        name={username}
-        tag={tag}
-        location={location}
-        image={avatar}
-        stats={stats}
+    <div className={styles.container}>
+      <SearchBar onSearch={handleSearch} />
+      <div className={clsx(styles.content, styles.section)}>
+        <ErrorMessage isError={error} />
+        <ImageGallery images={items} openModal={openModal} />
+        <Loader isLoading={loading} />
+        <LoadMoreBtn
+          isVisible={hasMorePages && !loading}
+          onClick={handleMore}
+        ></LoadMoreBtn>
+      </div>
+      <ImageModal
+        isOpen={modalIsOpen}
+        image={selectedImage}
+        onCloseClick={closeModal}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
     </div>
   );
-}
+};
+
+export default App;
