d66ed0ee6302e683c24160602aed0f5bd99a9b1d
Julietro29 <mousemiu1@gmail.com>
2024-03-30 19:08:58 +0200

    Add onClick

--- src/components/ImageGallery/ImageCard.jsx
+++ src/components/ImageGallery/ImageCard.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import styles from './ImageCard.module.css';
 
 export const ImageCard = ({ image: { urls, description } }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleClick}>
       <div className={styles.imgContainer}>
         <img className={styles.img} src={urls.small} alt={description} />
-        <div className={styles.imgDarkened}></div>
+        {isClicked && <div className={styles.imgDarkened}></div>}
       </div>
     </div>
   );
